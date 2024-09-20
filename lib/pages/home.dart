d17f726a98a1bf17088ddcd905afdd2a0bc25eff
akshkin <akshaya.kini12@gmail.com>
2024-09-20 21:52:20 +0200

    Add page for completed todos

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:todo/models/todo.dart';
 import 'package:todo/pages/add.dart';
+import 'package:todo/pages/completed.dart';
 import 'package:todo/providers/todo.provider.dart';
 
 class MyHomePage extends ConsumerWidget {
@@ -11,40 +12,57 @@ class MyHomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     List<Todo> todos = ref.watch(todoProvider);
+    List<Todo> activeTodos =
+        todos.where((todo) => todo.completed == false).toList();
+    List<Todo> completedTodos =
+        todos.where((todo) => todo.completed == true).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo App'),
       ),
       body: ListView.builder(
-          itemCount: todos.length,
+          itemCount: activeTodos.length + 1,
           itemBuilder: (context, index) {
-            return Slidable(
-                startActionPane:
-                    ActionPane(motion: const ScrollMotion(), children: [
-                  SlidableAction(
-                    onPressed: (context) {
-                      ref.watch(todoProvider.notifier).deleteTodo(index);
-                    },
-                    backgroundColor: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
-                    icon: Icons.delete,
-                  )
-                ]),
-                endActionPane:
-                    ActionPane(motion: const ScrollMotion(), children: [
-                  SlidableAction(
-                    onPressed: (context) {
-                      ref.watch(todoProvider.notifier).completedTodo(index);
-                    },
-                    backgroundColor: Colors.green,
-                    borderRadius: BorderRadius.circular(10),
-                    icon: todos[index].completed
-                        ? Icons.check_circle
-                        : Icons.check_box_outline_blank,
-                  )
-                ]),
-                child: ListTile(title: Text(todos[index].content)));
+            if (index == activeTodos.length) {
+              if (completedTodos.isEmpty) {
+                return Container();
+              } else {
+                return Center(
+                    child: TextButton(
+                        onPressed: () => Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => const CompletedTodos())),
+                        child: const Text("Completed todos")));
+              }
+            } else {
+              return Slidable(
+                  startActionPane:
+                      ActionPane(motion: const ScrollMotion(), children: [
+                    SlidableAction(
+                      onPressed: (context) {
+                        ref.watch(todoProvider.notifier).deleteTodo(index);
+                      },
+                      backgroundColor: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                      icon: Icons.delete,
+                    )
+                  ]),
+                  endActionPane:
+                      ActionPane(motion: const ScrollMotion(), children: [
+                    SlidableAction(
+                      onPressed: (context) {
+                        ref.watch(todoProvider.notifier).completedTodo(index);
+                      },
+                      backgroundColor: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                      icon: todos[index].completed
+                          ? Icons.check_circle
+                          : Icons.check_box_outline_blank,
+                    )
+                  ]),
+                  child: ListTile(title: Text(activeTodos[index].content)));
+            }
             // }
           }),
       floatingActionButton: FloatingActionButton(
