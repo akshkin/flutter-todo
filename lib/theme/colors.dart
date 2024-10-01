018300310fa7c2ffcdbaed2df0353db079b4d87d
akshkin <akshaya.kini12@gmail.com>
2024-10-01 11:54:07 +0200

    Create a constants file for colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorPallete {
+  static const blueLight = Color.fromARGB(255, 208, 234, 250);
+  static const greenLight = Color.fromARGB(255, 230, 250, 231);
+  static const blueDark = Color.fromARGB(255, 39, 177, 223);
+  static const greenDark = Color.fromARGB(255, 97, 206, 100);
+  static const textDark = Colors.black;
+  static const textGray = Color.fromARGB(255, 125, 123, 123);
+  static const red = Color.fromARGB(255, 239, 90, 80);
+}
